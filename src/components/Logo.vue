87defa887a63dc52447459ea767be99039c37de6
Ondrej Sevcik <Ondrej Sevcik>
2020-06-09 23:22:49 +0200

    added sample mock api, now change it to movies api

---
+++ src/components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <img alt="Logo" max-width="256px" src="@/assets/logo.png" />
+    <h2>Movies Databsase</h2>
+  </v-container>
+</template>
+
+<style scoped>
+img {
+  max-width: 192px;
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Logo />
+    <MoviesTable />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Logo from '@/components/Logo';
+import MoviesTable from '@/components/MoviesTable.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Logo,
+    MoviesTable,
   },
 };
 </script>
